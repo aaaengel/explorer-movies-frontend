d3ad0b39d536c2a690ae77b2503d45214a011728
Danila Engel <engeldanila@mail.ru>
2021-04-15 15:25:23 +0300

    fixes

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -5,12 +5,12 @@ function SearchForm(){
     return(
         <div className="movies__search">
         <form className="movies__search-form">
-          <input placeholder="Фильм" type="search" className="movies__input" />
+          <input placeholder="Фильм" type="search" className="movies__input" required />
           <button type="submit" className="movies__search-icon_right">
             Найти
           </button>
           </form>
-        <input type="checkbox" className="movies__checkbox" id="checkbox1"></input>
+        <input type="checkbox" className="movies__checkbox" id="checkbox1" ></input>
         <label for="checkbox1" className="movies__checkbox-label">Короткометражки</label>
       </div>
     )
